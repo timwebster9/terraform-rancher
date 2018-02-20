13c0c38a31a17cf98fac3bb6940d8b3e0cf16c18
Chris Vest <christopher.vest@wipro.com>
2018-02-20 15:58:56 +0000

    Remove 'tags' from subnet

--- main.tf
+++ main.tf
@@ -49,9 +49,6 @@ resource "azurerm_subnet" "test" {
   resource_group_name  = "${azurerm_resource_group.test.name}"
   virtual_network_name = "${azurerm_virtual_network.test.name}"
   address_prefix       = "10.0.2.0/24"
-  tags {
-    environment = "sales_test_rancher"
-  }
 }
 
 resource "azurerm_network_interface" "test" {
